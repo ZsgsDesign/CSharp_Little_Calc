94e56b1f97c86b5a751df4fd91481d379ac783a8
ZsgsDesign <zsgsdesign@gmail.com>
2018-12-25 21:48:16 +0800

    upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace C_
+{
+    class Program
+    {
+        static int seqSum(int n)
+        {
+            int sum=0;
+            for(int i=1;i<=n;i++)
+            {
+                sum+=i;
+            }
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Please input a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please input b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            multi multipler=new multi();
+            multipler.puts(a,b);
+            Console.Write("\na*b = ");
+            Console.WriteLine(multipler.gets());
+            Console.WriteLine(seqSum(100));
+        }
+    }
+
+    class multi
+    {
+        int a;
+        int b;
+
+        public void puts(int temp_a, int temp_b)
+        {
+            a = temp_a;
+            b = temp_b;
+        }
+
+        public int gets()
+        {
+            return a * b;
+        }
+    }
+}
